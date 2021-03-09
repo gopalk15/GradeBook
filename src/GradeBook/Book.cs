a9fa7ce47c2ed1a17d01a1eb31745bb642f91f97
Kaylin Gopal <kaylingopal@gmail.com>
2021-03-09 11:02:17 +0200

    Gradebook Get stats plus unit tests

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook
+{
+    public class Book
+    {
+        private List<double> grades;
+        private string bookName;
+
+        public Book(string name)
+        {
+            grades = new List<double>();
+            bookName = name;
+        }
+
+        public void AddGrade(double studentGrade)
+        {
+            grades.Add(studentGrade);
+        }
+
+        public Statistics GetStatistics()
+        {
+            Statistics result = new Statistics();
+
+            result.Average = 0.0;
+            result.Low = double.MaxValue;
+            result.High = double.MinValue;
+
+            foreach (double grade in grades)
+            {
+                //Compute sum 
+                result.Average += grade;
+                //Find lowest Grade
+                result.Low = Math.Min(grade, result.Low);
+                //Find higest Grade
+                result.High = Math.Max(grade, result.High);
+            }
+
+            //Compute Average 
+            result.Average /= grades.Count;
+
+            return result;
+        }
+    }
+}
---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book("Kaylin's Grade Book");
+
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
+            Statistics stats = book.GetStatistics();
+            Console.WriteLine($"The low grade is {stats.Low}");
+            Console.WriteLine($"The high grade is {stats.High}");
+            Console.WriteLine($"The average grade is {stats.Average}");
+
+
+
+
+
+        }
+    }
+}
